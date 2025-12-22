25b47df152973ce0755e820c59c3284b1e2d90ae
AishwaryaBhattbhatt <aishwarya.bhattbhatt@cbc.ca>
2025-12-22 15:43:35 -0500

    added new tokens

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,11 @@ export default defineConfig({
   // Replace 'mtm-website' with your actual new GitHub repo name
   base: '/mtm-website/',
   site: 'https://aishwaryabhattbhatt-cbc.github.io',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'], // Add the languages you want
+    routing: {
+        prefixDefaultLocale: false // If false, 'en' stays at / and others at /fr/
+    }
+  }
 });
